cfd65b020cd482b1c000570bc69597bb413fc51e
Charanjit Chana <cchana@gmail.com>
2023-11-20 13:57:56 +0000

    fixing indentation for routes.php

--- app/conf/routes.php
+++ app/conf/routes.php
@@ -14,36 +14,32 @@
  **/
 
 $routes = [
-    '/' => [
-        'controller' => 'documentation',
-        'method' => 'home',
-        'params' => [
-            'test'
-        ]
-    ],
-    '/examples' => [
-        'controller' => 'documentation',
-        'method' => 'examples'
-    ],
-    '/changelog' => [
-        'controller' => 'documentation',
-        'method' => 'changelog'
-    ],
-    '/readdocs' => [
-        'controller' => 'documentation',
-        'method' => 'readdocs'
-    ],
-    '/foo' => [
-        'controller' => 'test',
-        'method' => 3
-    ],
-    '/bar' => [
-        'controller' => 'test',
-        'method' => 3,
-        'params' => [
-            'predefinedparam'
-        ]
-    ]
+           '/'          => [
+                            'controller' => 'documentation',
+                            'method' => 'home',
+                            'params' => ['test'],
+                           ],
+           '/examples'  => [
+                            'controller' => 'documentation',
+                            'method' => 'examples',
+                           ],
+           '/changelog' => [
+                            'controller' => 'documentation',
+                            'method' => 'changelog',
+                           ],
+           '/readdocs'  => [
+                            'controller' => 'documentation',
+                            'method' => 'readdocs',
+                           ],
+           '/foo'       => [
+                            'controller' => 'test',
+                            'method' => 3,
+                           ],
+           '/bar'       => [
+                            'controller' => 'test',
+                            'method' => 3,
+                            'params' => ['predefinedparam'],
+                           ]
 ];
 
 define('ROUTES', $routes);
