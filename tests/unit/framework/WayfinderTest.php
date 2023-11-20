a2dfd91e8a3d75c650b09b5e47a563ec61c99215
Charanjit Chana <cchana@gmail.com>
2023-11-20 15:51:16 +0000

    Update WayfinderTest.php

--- tests/unit/framework/WayfinderTest.php
+++ tests/unit/framework/WayfinderTest.php
@@ -1,8 +1,8 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
-$unitTestPathPrefix = '';
-if (PHPUNIT !== null || PHPUNIT === false) {
+$unitTestPathPrefix = '/..';
+if (PHPUNIT === null || PHPUNIT === false) {
     $unitTestPathPrefix = '/../../..';
 }
 
